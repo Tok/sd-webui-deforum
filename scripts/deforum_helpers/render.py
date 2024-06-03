692f53e048a141352b91c99d095304a66879e9b2
Zir Teq <zirteq111@gmail.com>
2024-06-04 00:43:21 +0200

    Invalid opts ref fixed.

--- scripts/deforum_helpers/render.py
+++ scripts/deforum_helpers/render.py
@@ -581,13 +581,14 @@ def assign_masks(init, i, is_mask_image, dicts):
 
 
 def setup_opts(init, schedule):
+    data = init.args.opts.data
     if init.has_img2img_fix_steps():
         # disable "with img2img do exactly x steps" from general setting, as it *ruins* deforum animations
-        init.opts.data["img2img_fix_steps"] = False
-    put_if_present(init.args.opts.data, "CLIP_stop_at_last_layers", schedule.clipskip)
-    put_if_present(init.args.opts.data, "initial_noise_multiplier", schedule.noise_multiplier)
-    put_if_present(init.args.opts.data, "eta_ddim", schedule.eta_ddim)
-    put_if_present(init.args.opts.data, "eta_ancestral", schedule.eta_ancestral)
+        data["img2img_fix_steps"] = False
+    put_if_present(data, "CLIP_stop_at_last_layers", schedule.clipskip)
+    put_if_present(data, "initial_noise_multiplier", schedule.noise_multiplier)
+    put_if_present(data, "eta_ddim", schedule.eta_ddim)
+    put_if_present(data, "eta_ancestral", schedule.eta_ancestral)
 
 
 def progress_and_make_preview(init, image, frame_idx, state, last_preview_frame):
